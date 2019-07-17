6865569b571229c0e16bfc28b03ca8eeb879e54b
Anthony Rurup <tcrurup@gmail.com>
2019-07-17 22:54:53 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
+
+  def create
+    Post.create(title: params[:post][:title], description: params[:post][:description])
+    redirect_to posts_path
+  end
+
   def index
     @posts = Post.all
   end
