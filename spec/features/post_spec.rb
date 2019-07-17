c081566f6065bc9fb7211ee31964d9977488a8ae
Anthony Rurup <tcrurup@gmail.com>
2019-07-17 22:40:28 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,1 +1,8 @@
-a
+require 'rails_helper'
+ 
+describe 'new post' do
+  it 'ensures that the form route works with the /new action' do
+    visit new_post_path
+    expect(page.status_code).to eq(200)
+  end
+end
